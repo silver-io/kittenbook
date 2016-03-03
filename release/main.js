0fad1ec729380b98fc5c230bed684e16bf2f9a61
javier <javier@silver.io>
2016-03-03 17:11:33 +0000

    first commit

---
+++ release/main.js
@@ -0,0 +1,14 @@
+var projectName = 'kittenbook';
+var versionNumber = '0.0.1';
+var currentDate = new Date();
+var currentTime = currentDate.getFullYear() + '-' +
+					(currentDate.getMonth() + 1)+ '-' +
+					currentDate.getDate() + ' at ' +
+					currentDate.getHours() + ':' +
+					currentDate.getMinutes() + ':' +
+					currentDate.getSeconds();
+
+var userName = prompt('Hello what\'s your name?');
+document.body.innerHTML = '<h1>Hello,  ' + userName + '!</h1>' +
+							'<p>' + projectName + ' ' + versionNumber +
+							' accessed on: ' + currentTime + '</p>';
