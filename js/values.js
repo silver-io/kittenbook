cddbd2273ce99f983d31ccf3b84328e8f4404a48
javier <javier@silver.io>
2016-08-08 23:56:24 +0100

    creates an object in values

--- js/values.js
+++ js/values.js
@@ -1,9 +1,15 @@
-var projectName = 'kittenbook';
-var versionNumber = '0.0.1';
+var kbValues = {
+	projectName: 'kittenbook',
+	versionNumber: '0.0.1'
+};
 var currentDate = new Date();
-var currentTime = currentDate.getFullYear() + '-' +
-					(currentDate.getMonth() + 1)+ '-' +
-					currentDate.getDate() + ' at ' +
-					currentDate.getHours() + ':' +
-					currentDate.getMinutes() + ':' +
-					currentDate.getSeconds();
+
+var currentTime = currentDate.getFullYear() + '-' + 
+                  (currentDate.getMonth() + 1)+ '-' +
+                  currentDate.getDate() + ' at ' +
+                  currentDate.getHours() + ':' +
+                  currentDate.getMinutes() + ':' +
+                  currentDate.getSeconds();
+
+kbValues.currentTime = currentTime;
+ 
--- release/main.js
+++ release/main.js
@@ -1,14 +1,20 @@
-var projectName = 'kittenbook';
-var versionNumber = '0.0.1';
+var kbValues = {
+	projectName: 'kittenbook',
+	versionNumber: '0.0.1'
+};
 var currentDate = new Date();
-var currentTime = currentDate.getFullYear() + '-' +
-					(currentDate.getMonth() + 1)+ '-' +
-					currentDate.getDate() + ' at ' +
-					currentDate.getHours() + ':' +
-					currentDate.getMinutes() + ':' +
-					currentDate.getSeconds();
+
+var currentTime = currentDate.getFullYear() + '-' + 
+                  (currentDate.getMonth() + 1)+ '-' +
+                  currentDate.getDate() + ' at ' +
+                  currentDate.getHours() + ':' +
+                  currentDate.getMinutes() + ':' +
+                  currentDate.getSeconds();
+
+kbValues.currentTime = currentTime;
+ 
 
 var userName = prompt('Hello what\'s your name?');
 document.body.innerHTML = '<h1>Hello,  ' + userName + '!</h1>' +
-							'<p>' + projectName + ' ' + versionNumber +
-							' accessed on: ' + currentTime + '</p>';
+							'<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
+							' accessed on: ' + kbValues.currentTime + '</p>';
